da9da9284ce64b551ec2cd8f73b651dc78cbf261
picstare <125844636+picstare@users.noreply.github.com>
2023-03-19 12:18:20 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -47,8 +47,13 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
-            .viewerBadge_link__1S137 {visibility: hidden;} 
-            
+            viewerBadge_link__1S137 {visibility: hidden;} 
+            viewerBadge_link__ {visibility: hidden;}
+            viewerBadge_link {visibility: hidden;}
+            .viewerBadge_link__ {visibility: hidden;}
+            .viewerBadge_link__1S137 {visibility: hidden;}
+            .viewerBadge_link {visibility: hidden;}
+
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
